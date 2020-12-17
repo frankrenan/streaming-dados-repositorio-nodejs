d5e24e7c638eef7a125de0d767679bf85b70ff76
frank.lima <frank.lima@tce.am.gov.br>
2020-12-17 13:49:12 -0400

    Inserindo PATH extensao dos arquivos

--- archive/arquivoUpload.js
+++ archive/arquivoUpload.js
@@ -1,13 +1,27 @@
 const fs = require('fs'); // modulo de arquivo
-
+const path = require('path');
 
 
 module.exports = (caminho, nomeArquivo, callbackImagemCriada) => {
 
-    const novoCaminho = `./asset/img/${nomeArquivo}`;
+    const tipo = path.extname(caminho);
+    const tipoValidos = ['jpg', 'jpeg', 'png'];
+    const valido = tipoValidos.indexOf(tipo.substring(1));
 
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+    if (valido == -1) {
+        console.log('extensão não disponível')
+    }
+    else {
+        
+        const novoCaminho = `./asset/img/${nomeArquivo}${tipo}`;
+
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho))
+
+        console.log('salvo com sucesso');
+    }
+
+
 
 }
